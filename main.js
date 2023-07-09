4e95d718691bfa57cab05acd8d52bc3ec0a51edd
FRANCISCO <pachonetworking@gmail.com>
2023-07-08 23:05:07 -0500

    regExp object funtional

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 function telephoneCheck(str) {
-  const phoneNumbers = str.split("");
-  const onlyNumbers = phoneNumbers.filter((item) => item === "5");
-  console.log(onlyNumbers);
-  const onlyNoNumbers = phoneNumbers.filter((item) => item === "-");
-  console.log(onlyNoNumbers);
-
-  // return true;
+  const numberConditions = {
+    regExpNumber2: /[0-9]/gi,
+  };
+  // console.log(numberConditions.regExpNumber2.source)
+  // const regExpNumber = /[0-9]/gi;
+  // let prueba = regExpNumber.exec(str);
+  // let prueba2 = regExpNumber.test(str);
+  let prueba3 = numberConditions.regExpNumber2.test(str);
+  console.log(prueba3);
+  // if (prueba2) {
+  //   console.log(true)
+  //   return true
+  // } else {
+  //   console.log(false)
+  //   return false
+  // }
 }
 
 telephoneCheck("555-555-5555");
